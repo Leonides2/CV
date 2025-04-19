7f9438b38ccc1fdb4326bed25e796b542d4817c2
José Leonardo Zúñiga Hernández <leonidas12zh@gmail.com>
2025-04-18 20:39:50 -0600

    added contact part and refactor css from tailwindcss to do more responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,67 @@ import bgImage from "./assets/stone_embedded_tiles_nor_gl_1k.jpg"
 function App() {
 
   return (
-    <div className={`bg-[url(${bgImage})] bg-repeat grayscale-100}`}>
+    <div className={`bg-[url(${bgImage})] bg-repeat`}>
 
-      <div className='flex flex-col items-center justify-center h-screen 
-    bg-gradient-to-r 
-    from-indigo-500/80 
-    to-purple-500/80 
+      <div className='flex flex-col gap-10 items-center justify-center p-10 
+      h-auto
+      min-h-dvh
+      bg-fixed bg-cover bg-no-repeat bg-center 
+      bg-gradient-to-r 
+      from-indigo-500/80 
+      to-purple-500/80 
     '>
 
-        <div className="card flex flex-col items-center justify-center
-        bg-white/10 backdrop-blur-[1px] p-10 rounded-lg m-2 w-3/4">
-          <div className="flex items-center justify-center w-24 h-24 
-        rounded-full
-         bg-white/10 backdrop-blur-[1px] m-2">
-            <img src={profile.avatar} alt="avatar" className="rounded-full w-full h-full" />
+        <div className="grid grid-cols-2 grid-rows-1 items-center justify-center
+        bg-white/10 backdrop-blur-[3px] p-10 rounded-lg  w-3/4 min-h-96
+        
+        max-md:flex max-md:flex-col max-md:grid-cols-none"
+          style={{
+            gridTemplateColumns: "1fr 30%",
+            gridTemplateRows: "1fr",
+          }}
+        >
+          <div className="flex flex-col items-center justify-center">
+            <h1 className='text-2xl font-bold text-zinc-200'> {profile.name}</h1>
+            <h2 className='text-lg font-bold text-zinc-200'> {profile.title}</h2>
           </div>
-          <h1 className='text-2xl font-bold'> {profile.name}</h1>
+
+          <div className="container flex items-center justify-center h-full w-full 
+         bg-white/90 rounded-lg m-2 p-5">
+
+            <div
+              className=" container w-full h-full  bg-cover bg-no-repeat bg-center rounded-lg "
+              style={{
+                backgroundImage: `linear-gradient(to bottom, transparent 80%, rgba(255,255,255,0.9)), url(${profile.avatar})`,
+              }}
+            ></div>
+          </div>
+
+        </div>
+
+        <div className="container flex flex-col items-center justify-center
+        bg-white/30 backdrop-blur-lg p-10 rounded-lg w-3/4 ">
+
+          <div className='container flex flex-col items-center justify-center'>
+            <h1 className='text-2xl font-bold text-zinc-200'> {info.contact.title}</h1>
+
+
+            <div className=" container flex flex-col items-center justify-center
+            bg-white/30 p-10 rounded-lg w-full">
+
+              <div className='flex flex-col items-center justify-center w-full'>
+                <h1 className='text-xl font-bold text-zinc-200'> Email: {info.contact.email}</h1>
+                
+              </div>
+
+              <div className='flex flex-col items-center justify-center'>
+                <h1 className='text-xl font-bold text-zinc-200'> Phone: {info.contact.phone}</h1>
+              </div>
+              
+
+            </div>
+          </div>
+
         </div>
 
         <div className="flex flex-col items-center justify-center
