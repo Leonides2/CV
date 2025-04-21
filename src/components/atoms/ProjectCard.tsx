2b999eec135ebcb268950f2d73d1dc76267adecc
José Leonardo Zúñiga Hernández <leonidas12zh@gmail.com>
2025-04-21 15:28:27 -0600

    refactor: update ProjectCard layout and styling for improved responsiveness

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -14,21 +14,20 @@ const ProjectCard = (ob: projectcardprops) => {
     const dynamicLink = new URL(ob.link, import.meta.url).href;
 
     return (
-        <div className="flex flex-col justify-center rounded-lg bg-white/20"
-            style={{
+        <div className="flex flex-col justify-center rounded-lg bg-white/20 w-1/3 max-lg:w-full"
+            style={/*{
                 height: "460px",
                 width: "400px"
-            }}
+            }*/ undefined}
         >
+            <div className="overflow-clip w-full">
             <img className="rounded-t-lg" alt={`${ob.name + " img"} `} src={ob.image}
-                style={{
-                    height: "200px"
-                }}
             />
+            </div>
             <div className="h-full p-5 flex flex-col">
                 <h3 className=" text-white  font-bold text-lg text-wrap">{ob.name}</h3>
                 <p className="text-white/80 font-semibold text-wrap text-ellipsis">{ob.description}</p>
-                <a className="text-white/60 text-wrap">{ob.link}</a>
+                <a className="text-white/60 text-wrap wrap-break-word">{ob.link}</a>
                 <Button text="Click Here" callback={() => window.open(ob.link.startsWith("/") ? dynamicLink : ob.link)} />
             </div>
         </div>
